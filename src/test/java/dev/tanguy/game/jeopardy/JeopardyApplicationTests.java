fb8ee8f924762564ca887cef1e31bb9ab93cf035
TanguyGeraedts <tanguy.geraedts4@gmail.com>
2026-09-04 07:25:20 +0200

    innit

---
+++ src/test/java/dev/tanguy/game/jeopardy/JeopardyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tanguy.game.jeopardy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeopardyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
